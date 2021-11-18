a367bc348360838786291a881d80245f1b9c4ecb
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2021-11-18 19:58:34 +0530

    2: Integrated hilt

--- app/src/main/java/com/sanket/androidtestingrepo/LoginActivity.kt
+++ app/src/main/java/com/sanket/androidtestingrepo/LoginActivity.kt
@@ -1,9 +1,15 @@
 package com.sanket.androidtestingrepo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
+
+    private val viewModel: LoginViewModel by lazy { ViewModelProvider(this).get(LoginViewModel::class.java) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
