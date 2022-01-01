e24a225103bc65c711e34e9a645dc73495f0ecd4
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2022-01-01 14:55:26 +0530

    7: Added positive flow unit test for LoginViewModel

---
+++ app/src/test/java/com/sanket/androidtestingrepo/LoginViewModelTest.kt
@@ -0,0 +1,54 @@
+package com.sanket.androidtestingrepo
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
+import org.mockito.*
+import org.mockito.kotlin.capture
+import org.mockito.kotlin.eq
+import org.powermock.modules.junit4.PowerMockRunner
+
+@RunWith(PowerMockRunner::class)
+class LoginViewModelTest {
+
+    private lateinit var loginViewModel: LoginViewModel
+
+    @Mock
+    private lateinit var mockRepository: AuthRepository
+
+    @Mock
+    private lateinit var mockLoginObserver: Observer<SimpleResource>
+
+    @Rule
+    val testInstantTaskExecutorRule: TestRule = InstantTaskExecutorRule()
+
+    @Captor
+    private lateinit var responseCaptor: ArgumentCaptor<ResponseCallback<SimpleResource>>
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.openMocks(this)
+        loginViewModel = LoginViewModel(mockRepository)
+    }
+
+    @Test
+    fun givenCorrectUsernameAndPasswordShouldReturnSuccess() {
+        val username = "Sanket"
+        val password = "Sanket"
+
+        loginViewModel.getLoginLD().observeForTesting(mockLoginObserver) {
+            loginViewModel.login(username, password)
+            Mockito.verify(mockLoginObserver).onChanged(Resource.Loading)
+            Mockito.verify(mockRepository).login(eq(username), eq(password), capture(responseCaptor))
+            val successResponse = Resource.Success(Unit)
+            responseCaptor.value.onResponse(successResponse)
+            Mockito.verify(mockLoginObserver).onChanged(successResponse)
+        }
+    }
+
+}
---
+++ app/src/test/java/com/sanket/androidtestingrepo/TestExtension.kt
@@ -0,0 +1,5 @@
+package com.sanket.androidtestingrepo
+
+import org.mockito.ArgumentCaptor
+
+//fun <T> capture(argumentCaptor: ArgumentCaptor<T>): T = argumentCaptor.capture()
